f2602b9b96ddafbd81e84d592c896445b9ec3cc5
PitPed <srpitped@gmail.com>
2024-02-06 14:04:26 +0100

    SOAP server incompleto

--- controlador/SOAP.php
+++ controlador/SOAP.php
@@ -10,20 +10,18 @@
 
     session_start();
 
-
-
-    include("vista/menu.php");
-
-    const DEFAULT_VIEW = "usuario";
-
-    $views = array("usuario" => "vista/usuario.php", "animal" => "vista/animal.php", "adopcion" => "vista/adopcion.php");
-
     //Creación de "dummies"
     $animalDummy = new Animal('33', 'Garfield', 'Gato', 'Gato Naranja', 'Macho', 'Naranja', '27');
 
     $usuarioDummy = new Usuario('77', 'Paco', 'Suárez', 'Masculino', 'Calle Grande 4', '987654321');
 
     $adopcionDummy = new Adopcion(50, 33, 77, "2000-01-01", "Se sentía solo");
+
+    function recuperarAdopciones()
+    {
+        global $adopcionDummy;
+        return $adopcionDummy->obtieneTodos();
+    }
 
     function crearAdopcion($id, $idAnimal, $idUsuario, $fecha, $razon)
     {
@@ -41,6 +39,12 @@
     {
         global $adopcionDummy;
         return $adopcionDummy->borrar($id);
+    }
+
+    function recuperarAnimales()
+    {
+        global $animalDummy;
+        return $animalDummy->obtieneTodos();
     }
 
     function crearAnimal($id, $nombre, $especie, $raza, $genero, $color, $edad)
@@ -61,6 +65,12 @@
         return $animalDummy->borrar($id);
     }
 
+
+    function recuperarUsuarios()
+    {
+        global $usuarioDummy;
+        return $usuarioDummy->obtieneTodos();
+    }
     function crearUsuario($id, $nombre, $apellido, $sexo, $direccion, $telefono)
     {
         $newUser = new Usuario($id, $nombre, $apellido, $sexo, $direccion, $telefono);
@@ -78,6 +88,25 @@
         global $usuarioDummy;
         return $usuarioDummy->borrar($id);
     }
+
+    $options = array('uri' => 'http://localhost/Servidor/Servicios/SOAP/Servidor.php');
+
+
+    $SOAPserver = new SoapServer(null, $options);
+    $SOAPserver->addFunction('recuperarAdopciones');
+    $SOAPserver->addFunction('crearAdopcion');
+    $SOAPserver->addFunction('actualizarAdopcion');
+    $SOAPserver->addFunction('borrarAdopcion');
+    $SOAPserver->addFunction('recuperarAnimales');
+    $SOAPserver->addFunction('crearAnimal');
+    $SOAPserver->addFunction('actualizarAnimal');
+    $SOAPserver->addFunction('borrarAnimal');
+    $SOAPserver->addFunction('recuperarUsuarios');
+    $SOAPserver->addFunction('crearUsuario');
+    $SOAPserver->addFunction('actualizarUsuario');
+    $SOAPserver->addFunction('borrarUsuario');
+    $SOAPserver->handle();
+
     ?>
 
 </body>
